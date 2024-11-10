c22a13489465933a9273b957c47289b73cd6a2e8
Naushikha <dev@naushikha.com>
2024-11-10 18:19:26 +0530

    Complete cleanup and refactor on WorkingAR

--- src/r3f-mindar-js/WorkingAR.jsx
+++ src/r3f-mindar-js/WorkingAR.jsx
@@ -1,4 +1,9 @@
-import { createContext, memo, useCallback, useContext, useEffect, useMemo, useRef, useState } from "react";
+import {
+    useCallback,
+    useEffect,
+    useRef,
+    useState,
+} from "react";
 import { Matrix4, Quaternion, Vector3 } from "three";
 import { Canvas, useLoader, useThree } from "@react-three/fiber";
 import Webcam from "react-webcam";
@@ -7,48 +12,61 @@ import { atom, useAtomValue, useSetAtom } from "jotai";
 import { GLTFLoader } from "three/examples/jsm/Addons.js";
 import { Environment } from "@react-three/drei";
 
-const ARContext = createContext();
 const anchorsAtom = atom({});
 const webcamReadyAtom = atom(false);
-const invisibleMatrix = new Matrix4().set(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+const invisibleMatrix = new Matrix4().set(
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    1
+);
 
-const AR = memo(function AR({
+function ARProvider({
     children,
-    imageTargets,
+    imageTargetURL,
     filterMinCF = null,
     filterBeta = null,
     warmupTolerance = null,
     missTolerance = null,
-    maxTrack,
+    maxTrack = 1,
     webcamRef,
     containerRef,
 }) {
-
     const { camera } = useThree();
-    // const [ready, setReady] = useState(false);
     const setAnchors = useSetAtom(anchorsAtom);
     const webcamReady = useAtomValue(webcamReadyAtom);
-
-
-    const arContext = useMemo(() => {
-        const controller = new ImageTargetController({
-            inputWidth: webcamRef.current.video.videoWidth,
-            inputHeight: webcamRef.current.video.videoHeight,
-            filterMinCF,
-            filterBeta,
-            warmupTolerance,
-            missTolerance,
-            maxTrack,
-        });
-        return { controller };
-    }, [filterMinCF, filterBeta, warmupTolerance, missTolerance, maxTrack, webcamRef.current.video.videoWidth, webcamRef.current.video.videoHeight]);
+    const controllerRef = useRef(null);
 
     const resize = useCallback(() => {
-        const { controller } = arContext;
+        console.log("Resizing view");
+
+        // This bit is very important!
+        webcamRef.current.video.setAttribute(
+            "width",
+            webcamRef.current.video.videoWidth
+        );
+        webcamRef.current.video.setAttribute(
+            "height",
+            webcamRef.current.video.videoHeight
+        );
 
         let vw, vh; // display css width, height
-        const videoRatio = webcamRef.current.video.videoWidth / webcamRef.current.video.videoHeight;
-        const containerRatio = containerRef.current.clientWidth / containerRef.current.clientHeight;
+        const videoRatio =
+            webcamRef.current.video.videoWidth / webcamRef.current.video.videoHeight;
+        const containerRatio =
+            containerRef.current.clientWidth / containerRef.current.clientHeight;
         if (videoRatio > containerRatio) {
             vh = containerRef.current.clientHeight;
             vw = vh * videoRatio;
@@ -56,16 +74,16 @@ const AR = memo(function AR({
             vw = containerRef.current.clientWidth;
             vh = vw / videoRatio;
         }
-        const proj = controller.getProjectionMatrix();
+        const proj = controllerRef.current.getProjectionMatrix();
 
         // TODO: move this logic to controller
         // Handle when phone is rotated, video width and height are swapped
-        const inputRatio = controller.inputWidth / controller.inputHeight;
+        const inputRatio = controllerRef.current.inputWidth / controllerRef.current.inputHeight;
         let inputAdjust;
         if (inputRatio > containerRatio) {
-            inputAdjust = webcamRef.current.video.width / controller.inputWidth;
+            inputAdjust = webcamRef.current.video.width / controllerRef.current.inputWidth;
         } else {
-            inputAdjust = webcamRef.current.video.height / controller.inputHeight;
+            inputAdjust = webcamRef.current.video.height / controllerRef.current.inputHeight;
         }
         let videoDisplayHeight;
         let videoDisplayWidth;
@@ -74,13 +92,14 @@ const AR = memo(function AR({
             videoDisplayHeight *= inputAdjust;
         } else {
             videoDisplayWidth = containerRef.current.clientWidth;
-            videoDisplayHeight = videoDisplayWidth / controller.inputWidth * controller.inputHeight;
+            videoDisplayHeight =
+                (videoDisplayWidth / controllerRef.current.inputWidth) * controllerRef.current.inputHeight;
             videoDisplayHeight *= inputAdjust;
         }
         let fovAdjust = containerRef.current.clientHeight / videoDisplayHeight;
 
         // const fov = 2 * Math.atan(1 / proj[5] / vh * containerRef.clientHeight) * 180 / Math.PI; // vertical fov
-        const fov = 2 * Math.atan(1 / proj[5] * fovAdjust) * 180 / Math.PI; // vertical fov
+        const fov = (2 * Math.atan((1 / proj[5]) * fovAdjust) * 180) / Math.PI; // vertical fov
         const near = proj[14] / (proj[10] - 1.0);
         const far = proj[14] / (proj[10] + 1.0);
         const ratio = proj[5] / proj[0]; // (r-1) / (t -b)
@@ -88,47 +107,52 @@ const AR = memo(function AR({
         camera.fov = fov;
         camera.near = near;
         camera.far = far;
-        camera.aspect = containerRef.current.clientWidth / containerRef.current.clientHeight;
+        camera.aspect =
+            containerRef.current.clientWidth / containerRef.current.clientHeight;
         camera.updateProjectionMatrix();
 
-        webcamRef.current.video.style.top = (-(vh - containerRef.current.clientHeight) / 2) + "px";
-        webcamRef.current.video.style.left = (-(vw - containerRef.current.clientWidth) / 2) + "px";
+        webcamRef.current.video.style.top =
+            -(vh - containerRef.current.clientHeight) / 2 + "px";
+        webcamRef.current.video.style.left =
+            -(vw - containerRef.current.clientWidth) / 2 + "px";
         webcamRef.current.video.style.width = vw + "px";
         webcamRef.current.video.style.height = vh + "px";
-    }, [arContext, camera])
+    }, [controllerRef, containerRef, webcamRef, camera]);
 
-    // const onUmount = useCallback(() => {
-    //     window.removeEventListener("resize", resize);
-    // }, [resize]);
+    const startTracking = useCallback(async () => {
+        console.log("Starting AR...");
 
-    const startAR = useCallback(async () => {
-        console.log('👾 Start AR');
+        const controller = new ImageTargetController({
+            inputWidth: webcamRef.current.video.videoWidth,
+            inputHeight: webcamRef.current.video.videoHeight,
+            filterMinCF: filterMinCF,
+            filterBeta: filterBeta,
+            warmupTolerance: warmupTolerance,
+            missTolerance: missTolerance,
+            maxTrack: maxTrack,
+            onUpdate: (data) => {
+                if (data.type === "updateMatrix") {
+                    const { targetIndex, worldMatrix } = data;
 
-        const { controller } = arContext;
-
-        //////////////////////////////////////////////////////////////////////////////////////////
-        webcamRef.current.video.setAttribute('width', webcamRef.current.video.videoWidth);
-        webcamRef.current.video.setAttribute('height', webcamRef.current.video.videoHeight);
-        controller.inputWidth = webcamRef.current.video.videoWidth;
-        controller.inputHeight = webcamRef.current.video.videoHeight;
-        //////////////////////////////////////////////////////////////////////////////////////////
-
-        controller.onUpdate = (data) => {
-            if (data.type === 'updateMatrix') {
-                // console.log("detected")
-                const { targetIndex, worldMatrix } = data;
-
-                setAnchors((anchors) => ({
-                    ...anchors, [targetIndex]: worldMatrix !== null ?
-                        new Matrix4().fromArray([...worldMatrix]).multiply(postMatrixs[targetIndex]).toArray() :
-                        invisibleMatrix.toArray()
-                }));
+                    setAnchors((anchors) => ({
+                        ...anchors,
+                        [targetIndex]:
+                            worldMatrix !== null
+                                ? new Matrix4()
+                                    .fromArray([...worldMatrix])
+                                    .multiply(postMatrixs[targetIndex])
+                                    .toArray()
+                                : invisibleMatrix.toArray(),
+                    }));
+                }
             }
-        }
+        });
 
+        controllerRef.current = controller;
         resize();
 
-        const { dimensions: imageTargetDimensions } = await controller.addImageTargets(imageTargets);
+        const { dimensions: imageTargetDimensions } =
+            await controller.addImageTargets(imageTargetURL);
 
         let postMatrixs = [];
         for (let i = 0; i < imageTargetDimensions.length; i++) {
@@ -148,72 +172,43 @@ const AR = memo(function AR({
 
         await controller.dummyRun(webcamRef.current.video);
         controller.processVideo(webcamRef.current.video);
-    }, [arContext, imageTargets, resize]);
+        console.log("AR processing started.")
+    }, [imageTargetURL, resize]);
 
     const stopTracking = useCallback(() => {
-        const { controller } = arContext;
-        controller.stopProcessVideo();
-    }, [arContext]);
+        if (controllerRef.current) {
+            controllerRef.current.stopProcessVideo();
+        }
+    }, [controllerRef]);
 
     useEffect(() => {
-        // const loadedMetadataHandler = () => {
-        //     console.log('📹 Ready');
-        //     webcamRef.current.video.setAttribute('width', webcamRef.current.video.videoWidth);
-        //     webcamRef.current.video.setAttribute('height', webcamRef.current.video.videoHeight);
-        //     // const { controller } = arContext;
-        //     // controller.inputWidth = webcamRef.current.video.videoWidth;
-        //     // controller.inputHeight = webcamRef.current.video.videoHeight;
-        //     setReady(true);
-        //     console.log("???")
-        // };
-
         const resizeHandler = () => {
-            console.log('📐 Resize')
             resize();
         };
-
-
-        // webcamRef.current.video.addEventListener('loadedmetadata', loadedMetadataHandler);
         window.addEventListener("resize", resizeHandler);
-
         return () => {
-            // webcamRef.current.video.removeEventListener('loadedmetadata', loadedMetadataHandler);
             window.removeEventListener("resize", resizeHandler);
             stopTracking();
         };
-    }, [startAR]);
-
+    }, [startTracking]);
 
     useEffect(() => {
         if (webcamReady) {
-            startAR();
+            startTracking();
         }
-    }, [webcamReady, startAR])
+    }, [webcamReady, startTracking]);
 
-    const value = useMemo(() => ({ controller: arContext.controller }), [arContext])
+    return <>{children}</>;
+};
 
-    return <ARContext.Provider value={value}>{children}</ARContext.Provider>
-});
-
-// const useAR = () => {
-//     const arValue = useContext(ARContext)
-//     return useMemo(() => ({ ...arValue }), [arValue])
-// }
-
-function ARAnchor({
-    children,
-    target = 0,
-}) {
-    // const { controller } = useAR(); // Can we omit this?
+function ARAnchor({ children, target = 0 }) {
     const ref = useRef();
-    const anchors = useAtomValue(anchorsAtom)
+    const anchors = useAtomValue(anchorsAtom);
 
     useEffect(() => {
         if (ref.current) {
-            // if (controller.inputWidth === 0) {
-            //     return;
-            // }
-            if (anchors[target]) { // L#159
+            if (anchors[target]) {
+                // L#159
                 //if (ref.current.visible !== true && onAnchorFound) onAnchorFound();
                 ref.current.visible = true;
                 ref.current.matrix = new Matrix4().fromArray(anchors[target]);
@@ -222,26 +217,16 @@ function ARAnchor({
                 ref.current.visible = false;
             }
         }
-
-    }, [
-        // controller, 
-        anchors, target])
+    }, [anchors, target]);
 
     return (
         <group ref={ref} visible={false} matrixAutoUpdate={false}>
             {children}
         </group>
-    )
-
+    );
 }
 
-
-function ARCanvas({
-    children,
-    imageTargets,
-    filterMinCF,
-    filterBeta
-}) {
+function ARCanvas({ children, imageTargetURL, filterMinCF, filterBeta }) {
     const webcamRef = useRef();
     const canvasContainerRef = useRef();
     const setWebcamReady = useSetAtom(webcamReadyAtom);
@@ -249,34 +234,47 @@ function ARCanvas({
     const handleWebcam = useCallback(() => {
         if (webcamRef.current) {
             webcamRef.current.video.addEventListener("loadedmetadata", () => {
-                setWebcamReady(true)
-                console.log("Webcam is ready")
-            }
-            );
+                setWebcamReady(true);
+                console.log("Webcam is ready");
+            });
         }
     }, [webcamRef]);
 
     return (
-        <div id="ar-canvas-container" ref={canvasContainerRef} style={{ width: '100vw', height: '100vh', overflow: 'hidden', position: 'relative' }}>
-            <Canvas
-            >
-                {/* <Environment files="/metro_vijzelgracht_1k.hdr" /> */}
-                <AR imageTargets={imageTargets} webcamRef={webcamRef} containerRef={canvasContainerRef} filterMinCF={filterMinCF} filterBeta={filterBeta}>
+        <div
+            id="ar-canvas-container"
+            ref={canvasContainerRef}
+            style={{
+                width: "100vw",
+                height: "100vh",
+                overflow: "hidden",
+                position: "relative",
+            }}
+        >
+            <Canvas>
+                <Environment files="/metro_vijzelgracht_1k.hdr" />
+                <ARProvider
+                    imageTargetURL={imageTargetURL}
+                    webcamRef={webcamRef}
+                    containerRef={canvasContainerRef}
+                    filterMinCF={filterMinCF}
+                    filterBeta={filterBeta}
+                >
                     {children}
-                </AR>
+                </ARProvider>
             </Canvas>
             <Webcam
                 ref={webcamRef}
                 onUserMedia={handleWebcam}
                 style={{
-                    position: 'absolute',
+                    position: "absolute",
                     top: 0,
                     left: 0,
                     zIndex: -2,
                 }}
             />
         </div>
-    )
+    );
 }
 
 // for testing
@@ -298,25 +296,20 @@ function Car3D() {
 
 function ARParent() {
     return (
-        <ARCanvas
-            imageTargets="/car.mind"
-            filterMinCF={0.001}
-            filterBeta={0.001}
-        >
+        <ARCanvas imageTargetURL="/car.mind" filterMinCF={0.001} filterBeta={0.001}>
             <ambientLight />
             <pointLight position={[10, 10, 10]} />
             <ARAnchor target={0}>
-                {/* <Car3D /> */}
-                <mesh>
+                <Car3D />
+                {/* <mesh>
                     <boxGeometry args={[1, 1, 0.1]} />
                     <meshStandardMaterial color="orange" />
-                </mesh>
+                </mesh> */}
             </ARAnchor>
         </ARCanvas>
-    )
+    );
 }
 
 export default ARParent;
 
 // TODO: fix events; on anchor found/lost, ui loading/scanning etc.
-// TODO: remove useAR 
